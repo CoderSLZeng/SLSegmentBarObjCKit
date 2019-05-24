0a1bafeb6f93d2d9bf956203b41039285a0af693
CoderSLZeng <zeng@AmassdeMac-mini.local>
2019-05-24 13:38:10 +0800

    Initial commit

---
+++ Example/SLSegmentBarObjCKit/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SLSegmentBarObjCKit
+//
+//  Created by CoderSLZeng on 05/24/2019.
+//  Copyright (c) 2019 CoderSLZeng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/SLSegmentBarObjCKit/SLViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLViewController.h
+//  SLSegmentBarObjCKit
+//
+//  Created by CoderSLZeng on 05/24/2019.
+//  Copyright (c) 2019 CoderSLZeng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SLViewController : UIViewController
+
+@end
